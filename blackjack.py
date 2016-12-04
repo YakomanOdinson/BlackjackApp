745f0688eeefcaf410e22c8dd0bedbb39ea1bd22
Yakoman <gerardo.garcia.giacoman@gmail.com>
2016-12-04 15:24:56 -0600

    Revised the 2 files.

--- blackjack.py
+++ blackjack.py
@@ -121,6 +121,10 @@ def new_game():
 def shuffle():
     random.shuffle(deck)
 
+
+def yako():
+    print("Yako says hello")
+
 mainWindow = tkinter.Tk()
 
 # Set up the screen and frames for the dealer and player
